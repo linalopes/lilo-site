b73d679110324d83519fa4506711f9984ea0ef8e
Thiago Hersan <thiago.hersan+github@gmail.com>
2017-02-18 18:32:36 -0200

    [thiago] melhor logica para o filtro de eventos da home

--- js/event-filter.js
+++ js/event-filter.js
@@ -1,14 +1,13 @@
 $(function() {
   $(".breadcrumb-button").on("click",function() {
     // toggle underline
-    $(".breadcrumb-button span").css("border-bottom", "none");
-    $(this).find("span").css("border-bottom", "3px #ff0080 solid");
+    $("span.filter-active").removeClass("filter-active");
+    $(this).find("span").addClass("filter-active");
 
     if($(this).hasClass( "evento-ano-2017" )){
       $(".evento-item").show();
     } else {
-      //$(".evento-item").hide();
-      $(".evento-item").css("display", "none");
+      $(".evento-item").hide();
       $("."+$(this).attr('class').match(/evento\-.*/)[0]).show();
     }
   });
